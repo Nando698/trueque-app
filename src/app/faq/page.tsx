92bf2a87ca49ef9aa14a988c154ac2b1c5a5dbb2
Matias Fredes <112669190+matfredes@users.noreply.github.com>
2025-05-17 00:47:26 +0000

    Cambios layout

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,13 +1,9 @@
-import Header from '@/components/Header'
 import FAQContent from '@/components/FAQContent'
-import Footer from '@/components/Footer'
 
 export default function Page() {
   return (
     <>
-      <Header />
       <FAQContent />
-      <Footer />
     </>
 
 
